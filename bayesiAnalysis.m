055906af76da9f14ffee854b624dcf230db76141
nicoloc <nico.campolongo@gmail.com>
2016-05-15 20:15:19 +0200

    added plots

--- bayesiAnalysis.m
+++ bayesiAnalysis.m
@@ -9,7 +9,8 @@ x = linspace(1851, 1963, length(coal_mine));
 hyperParam = 1;
 breakpoints = 3;
 d = breakpoints+1;
-theta = gamrnd(2,1/hyperParam);
+ro = 0.1;
+acc = 0;
 
 %% gibbs sampling with MH step
 
@@ -28,17 +29,18 @@ t(:,1) = linspace(1851, 1963, d+1);
 % lambda intensities
 lambda = zeros(d,M);
 
+% theta parameter
+theta = zeros(1,M);
+
 % number of disasters
 nDis = zeros(d,M);
 
 % f(t)
 ft = @(lambda,t,n) prod(lambda.^n .* exp(-lambda.*diff(t)) .* diff(t));
-fun = @(x,y,z,n) (x.^n).*(y-z)*exp(-x.*(y-z));
-
-ro = 0.1;
-acc = 0;
 
 for j = 1:M-1
+    
+    theta(j) = gamrnd(2,1/hyperParam);
     
     % number of disaster update
     for i = 1:d
@@ -46,7 +48,7 @@ for j = 1:M-1
     end
     
     % lambda update
-    lambda(:,j) = gamrnd(nDis(:,j)+2, 1./(diff(t(:,j))+theta));
+    lambda(:,j) = gamrnd(nDis(:,j)+2, 1./(diff(t(:,j))+theta(j)));
     
     % MH step
     for l = 2:d                
@@ -70,35 +72,28 @@ for j = 1:M-1
         else
           t(l,j+1) = t(l,j);
         end
-    end
-    
-    %{
-    cand(j) = randWalkProp(ro, t(:,j));
-    t_star = t(:,j);
-    t_star(2) = cand(j);
-    
-    if cand(j) > t(1,j) && cand(j) < t(3,j)
-        for i = 1:d
-            nDis_star(i,1) = sum(coal_mine >= t_star(i) & coal_mine <= t_star(i+1));
-        end
-        num = ft(lambda(:,j), t_star, nDis_star);
-        den = ft(lambda(:,j), t(:,j), nDis(:,j));
-        alpha = num / den;
-        r = rand;
-        if r <= alpha
-            t(2,j+1) = cand(j);
-            acc = acc + 1;
-        else
-            t(2,j+1) = t(2,j);
-        end
-    end        
-    %}
-    
-    % theta update
-    theta = gamrnd(2*(d+1), 1/(hyperParam+sum(lambda(:,j))));
+    end  
+        
 end 
 
-tau = ceil(mean(t(:, burn_in:M),2))
+tau = ceil(mean(t(:, burn_in:M),2));
+
+%% displaying lambda intensities
+
+figure
+s = size(lambda,1);
+for i = 1:s
+    subplot(s,1,i)
+    h = histfit(lambda(i,:),50,'gamma');
+    h(1).FaceColor = [.6 .8 1];
+    str = sprintf('lambda %d', i);
+    title(str) 
+end    
+
+%% displaying theta intensities
+figure
+h = histfit(theta,50,'gamma');
+h(1).FaceColor = [.6 .8 1];
 
 %% statistics
 if size(t,1) == 3
