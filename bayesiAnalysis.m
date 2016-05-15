2ef47067f8ffbd107e85a8f07580990b244014ab
nicoloc <nico.campolongo@gmail.com>
2016-05-15 19:22:39 +0200

    added more than one breakpoint

--- bayesiAnalysis.m
+++ bayesiAnalysis.m
@@ -1,3 +1,4 @@
+clear
 load('coal_mine.mat')
 
 %% data visualization
@@ -6,7 +7,7 @@ x = linspace(1851, 1963, length(coal_mine));
 
 %% defining parameters
 hyperParam = 1;
-breakpoints = 1;
+breakpoints = 3;
 d = breakpoints+1;
 theta = gamrnd(2,1/hyperParam);
 
@@ -33,18 +34,45 @@ nDis = zeros(d,M);
 % f(t)
 ft = @(lambda,t,n) prod(lambda.^n .* exp(-lambda.*diff(t)) .* diff(t));
 fun = @(x,y,z,n) (x.^n).*(y-z)*exp(-x.*(y-z));
-%prod(lambda(:,j).^nDis(:,j).*exp(-lambda(:,j).*diff(t(:,j))).*diff(t(:,j)))
 
 ro = 0.1;
 acc = 0;
 
 for j = 1:M-1
+    
+    % number of disaster update
     for i = 1:d
-        nDis(i,j) = sum(coal_mine >= t(i,j) & coal_mine <= t(i+1,j));
-        %lambda(i,j) = gamrnd(nDis(i,j)+2, 1/(t(i+1,j)-t(i,j)+theta));
+        nDis(i,j) = sum(coal_mine >= t(i,j) & coal_mine <= t(i+1,j));        
     end
+    
+    % lambda update
     lambda(:,j) = gamrnd(nDis(:,j)+2, 1./(diff(t(:,j))+theta));
     
+    % MH step
+    for l = 2:d                
+        cand = randWalkProp(ro, [t(l-1,j) t(l,j) t(l+1,j)]);
+        t_star = t(:,j);
+        t_star(l) = cand;
+       
+        if cand > t(l-1,j) && cand < t(l+1,j);
+            for i = 1:d
+                nDis_star(i,1) = sum(coal_mine >= t_star(i) & ...
+                    coal_mine <= t_star(i+1));
+            end
+            alpha = ft(lambda(:,j), t_star, nDis_star) / ...
+                    ft(lambda(:,j), t(:,j), nDis(:,j));
+            if rand <= alpha
+                t(l,j+1) = cand;
+                acc = acc + 1;
+            else
+                t(l,j+1) = t(l,j);
+            end            
+        else
+          t(l,j+1) = t(l,j);
+        end
+    end
+    
+    %{
     cand(j) = randWalkProp(ro, t(:,j));
     t_star = t(:,j);
     t_star(2) = cand(j);
@@ -64,19 +92,17 @@ for j = 1:M-1
             t(2,j+1) = t(2,j);
         end
     end        
+    %}
     
-    %{
-    num = fun(lambda(2,j), t(3,j), cand(j), nDis(2,j));
-    den = fun(lambda(2,j), t(3,j), t(2,j), nDis(2,j));
-    alpha = num / den;
-    %}        
-    
+    % theta update
     theta = gamrnd(2*(d+1), 1/(hyperParam+sum(lambda(:,j))));
 end 
 
-tau = ceil(mean(t(2, burn_in:M)));
+tau = ceil(mean(t(:, burn_in:M),2))
 
 %% statistics
-histogram(cand)
-str=sprintf('acceptance rate = %.3f, tau = %d', acc/M, tau);
-title(str)
+if size(t,1) == 3
+    histogram(cand)
+    str=sprintf('acceptance rate = %.3f, tau = %d', acc/M, tau);
+    title(str)
+end
--- randWalkProp.m
+++ randWalkProp.m
@@ -2,13 +2,6 @@ function cand = randWalkProp(ro,breakpoints)
     % breakpoints contains t_i-1, t_i, t_i+1
     R = ro*(breakpoints(3)-breakpoints(1));
     eps = -R + (2*R)*rand;
-    cand = breakpoints(2) + eps;
-    %{
-    if breakpoints(2) + eps > breakpoints(1)
-        cand = breakpoints(2) + eps;
-    else
-        cand = breakpoints(2);
-    end
-    %}
+    cand = breakpoints(2) + eps;    
 
 
